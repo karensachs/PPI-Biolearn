c7541ac4dd0b7407c1605181ebfd3bcc9055216d
Karen Sachs <karensachs@karen-sachss-macbook-pro-2.local>
2020-04-19 22:33:04 -0400

    Organizing functions into directories

---
+++ PPI-biolearn/LINCS_PCSF_Constrained/Make_spec_PPIconstrained_v3.m
@@ -0,0 +1,98 @@
+ %v3 -bug fix Apr 1 2020 - BL expects directed constraints, so i need to put in both directions to block an edge
+%I also start with the Sp Candidate alg starterspec
+
+
+% spec_PPIconstrained creates a spec file which Biolearn will apply to create a PPI-constrained network.
+%The spec file (biolearn.spec) created by this program should be in the same directory when biolearn is run. If biolearn is run in Interactive Mode, it is also possible to browse to access the spec file.
+% As input, this program accepts ppi, a 2 column matrix indicating all
+% pairs of PPI edges,
+% It also accepts as input a networkname (text string),filelist, a textfile listing
+%the list of data files, and variable_names, a list of variables in the
+%order they appear in the data array(s)
+%Karen Sachs 2019
+
+function spec_PPIconstrained_v3(networkname,filelist,variable_names,ppi);
+
+%could add as input options:
+% filelistforspec,celltype,condindex,drugname,doselist,scoretype,discBuckets,numruns,numcells)
+
+% permitted=importdata(ppi_textfile.txt);
+% BLdir=fldir;
+
+%% make ppi into a matrix
+numvar=length(variable_names);
+ppiM=zeros(numvar,numvar);
+for i=1:size(ppi,1)
+    node1=ppi(i,1);
+    node2=ppi(i,2);
+    ppiM(node1,node2)=1;
+end
+
+%% Make spec file
+% DefaultNetworkName pcsfLINCS_constrained_BN
+% Score BDe 10.0
+% DiscretizationBuckets 3 SOFT BYDISTANCE
+% algorithm GreedyHillClimbing 5
+% dataformat DataPointPerLine 
+% sample 1
+% numruns 10
+% confidencethreshold 30
+% columnStatusFile biolearn.columnstatus.txt
+% constraint ParentMaximum 3
+% Prior EdgePenalty Fraction .99999
+% columnStatusFile biolearn.columnstatus.txt
+
+discBuckets=3;
+numruns=10;
+%         Specfilename=['biolearnPPI.spec'];
+        
+%         fid = fopen(Specfilename,'w+');
+        fid = fopen('starterspec_SCAlg.spec.txt', 'a+');
+%         fprintf(fid,'%s ','DefaultNetworkName ');
+%         fprintf(fid,'%s ',networkname);
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','discretescore BDe 5');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','VariableStatusFile biolearn.columnstatus.txt');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','algorithm GreedyHillClimbing 5');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','data MultipleInputFiles DataPointPerLine ');
+% %         fprintf(fid,'%s',fldir);
+% %         fprintf(fid,'%s ',filelist);%forspec);
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','sample ');
+%         fprintf(fid,'%s 1');%,num2str(numcells));
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','DiscretizationBuckets ');
+%         fprintf(fid,'%s ',num2str(discBuckets));
+%         fprintf(fid,'%s ',' soft bydistance');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','numruns ');
+%         fprintf(fid,'%s ',num2str(numruns));
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','confidencethreshold 50');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','discretizedFileSuffix discr');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','constraint ParentMaximum 4');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','Prior EdgePenalty Fraction .2');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','batch');
+        for i=1:numvar
+            for j=1:numvar %bug fix Apr 1 2020 - BL expects directed constraints, so i need to put in both directions to block an edge
+                if ppiM(i,j)==0 & ppiM(j,i)==0
+                    var1=variable_names{i};
+                    var2=variable_names{j};
+                    
+                    fprintf(fid,'%s ',['Constraint NoEdge ',var1, ' ',var2])
+                    fprintf(fid,'\n')
+                end
+            end
+        end
+        fprintf(fid,'\n');
+        fclose(fid);
+
+%%April 2019, Karen & Dan 
+% 'source' function: makeSpec from Documents/MATLAB/CYTOF/ComparativeSignaling/code/BL'
---
+++ PPI-biolearn/LINCS_PCSF_Constrained/spec_PPIconstrained_v2_justToCountConstraints.m
@@ -0,0 +1,119 @@
+
+%march 31 [Specfilename] = spec_PPIconstrained_v2_justToCountConstraints(networkname,filelist,variable_names,ppi);
+% BL is hanging (7 hours so far on first iteraration - so I'm checking that I didnt accidentally eliminate any possible edges :)
+% checks out correctly ount_edges =
+% 
+%        78606
+% 
+% 
+% count_constraints =
+% 
+%        77730
+% 
+% 78606-77730
+% 
+% ans =
+% 
+%    876
+% 
+
+
+% spec_PPIconstrained creates a spec file which Biolearn will apply to create a PPI-constrained network.
+%The spec file (biolearn.spec) created by this program should be in the same directory when biolearn is run. If biolearn is run in Interactive Mode, it is also possible to browse to access the spec file.
+% As input, this program accepts ppi, a 2 column matrix indicating all
+% pairs of PPI edges,
+% It also accepts as input a networkname (text string),filelist, a textfile listing
+%the list of data files, and variable_names, a list of variables in the
+%order they appear in the data array(s)
+%Karen Sachs 2019
+
+function spec_PPIconstrained_v2(networkname,filelist,variable_names,ppi);
+
+%could add as input options:
+% filelistforspec,celltype,condindex,drugname,doselist,scoretype,discBuckets,numruns,numcells)
+
+% permitted=importdata(ppi_textfile.txt);
+% BLdir=fldir;
+
+%% make ppi into a matrix
+numvar=length(variable_names);
+ppiM=zeros(numvar,numvar);
+for i=1:size(ppi,1)
+    node1=ppi(i,1);
+    node2=ppi(i,2);
+    ppiM(node1,node2)=1;
+end
+
+%% Make spec file
+% DefaultNetworkName pcsfLINCS_constrained_BN
+% Score BDe 10.0
+% DiscretizationBuckets 3 SOFT BYDISTANCE
+% algorithm GreedyHillClimbing 5
+% dataformat DataPointPerLine 
+% sample 1
+% numruns 10
+% confidencethreshold 30
+% columnStatusFile biolearn.columnstatus.txt
+% constraint ParentMaximum 3
+% Prior EdgePenalty Fraction .99999
+% columnStatusFile biolearn.columnstatus.txt
+
+discBuckets=3;
+numruns=10;
+%         Specfilename=['biolearnPPI.spec'];
+        
+%         fid = fopen(Specfilename,'w+');
+        fid = fopen('starterspec.txt', 'a+');
+%         fprintf(fid,'%s ','DefaultNetworkName ');
+%         fprintf(fid,'%s ',networkname);
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','discretescore BDe 5');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','VariableStatusFile biolearn.columnstatus.txt');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','algorithm GreedyHillClimbing 5');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','data MultipleInputFiles DataPointPerLine ');
+% %         fprintf(fid,'%s',fldir);
+% %         fprintf(fid,'%s ',filelist);%forspec);
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','sample ');
+%         fprintf(fid,'%s 1');%,num2str(numcells));
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','DiscretizationBuckets ');
+%         fprintf(fid,'%s ',num2str(discBuckets));
+%         fprintf(fid,'%s ',' soft bydistance');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','numruns ');
+%         fprintf(fid,'%s ',num2str(numruns));
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','confidencethreshold 50');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','discretizedFileSuffix discr');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','constraint ParentMaximum 4');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','Prior EdgePenalty Fraction .2');
+%         fprintf(fid,'\n');
+%         fprintf(fid,'%s ','batch');
+count_edges=0;
+count_constraints=0;
+        for i=1:numvar
+            for j=i+1:numvar
+                count_edges=count_edges+1;
+                if ppiM(i,j)==0 & ppiM(j,i)==0
+                    count_constraints=count_constraints+1;
+                    var1=variable_names{i};
+                    var2=variable_names{j};
+                    
+                    fprintf(fid,'%s ',['Constraint NoEdge ',var1, ' ',var2])
+                    fprintf(fid,'\n')
+                end
+            end
+        end
+        fprintf(fid,'\n');
+        fclose(fid);
+count_edges
+count_constraints
+%%April 2019, Karen & Dan 
+% 'source' function: makeSpec from Documents/MATLAB/CYTOF/ComparativeSignaling/code/BL'
